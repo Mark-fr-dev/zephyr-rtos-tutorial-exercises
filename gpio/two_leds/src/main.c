77ffa94b5f7993c04976ae755190ba833f1504be
Mark de Villiers <mfrdevilliers@gmail.com>
2025-11-12 07:51:41 +0800

    WIP: Testing sample code

--- gpio/two_leds/src/main.c
+++ gpio/two_leds/src/main.c
@@ -1,127 +1,124 @@
+/*
+ * Copyright (c) 2017 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/sys/__assert.h>
-#include <zephyr/string.h>
+#include <zephyr/sys/mem_manage.h>
+#include <string.h>
 
-/********************** */
-/*IO related definitions*/
-/********************** */
-#define SLEEP_TIME_MS 1000
-/* Devicetree node identifier for "led0" alias*/
-#define LED0_NODE DT_ALIAS(led0)
-#define LED1_NODE DT_ALIAS(led1)
-
-#if (!DT_NODE_HAS_STATUS(LED0_NODE, okay))
-#error "Unsupported board: led0 devicetree alias is not defined"
-#endif
-
-#if (!DT_NODE_HAS_STATUS(LED1_NODE, okay))
-#error "Unsupported board: led0 devicetree alias is not defined"
-#endif
-
-/*Extract GPIO information */
-static const struct gpio_dt_spec green_led = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
-static const struct gpio_dt_spec blue_led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-
-/********************** */
-/*Thread related definitions*/
-/********************** */
 /* size of stack area used by each thread */
 #define STACKSIZE 1024
 
 /* scheduling priority used by each thread */
 #define PRIORITY 7
 
-/* Running time for threads*/
-#define SLEEPTIME 500
-#define BLUE_PERIOD_MS 500
-#define GREEN_PERIOD_MS 2000
+#define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
 
-K_THREAD_STACK_DEFINE(blue_thread_stack_area, STACKSIZE);
-static struct k_thread blue_thread_data;
+#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#error "Unsupported board: led0 devicetree alias is not defined"
+#endif
 
-/* Define the stack area for thread B*/
-K_THREAD_STACK_DEFINE(green_thread_stack_area, STACKSIZE);
-static struct k_thread green_thread_data;
+#if !DT_NODE_HAS_STATUS(LED1_NODE, okay)
+#error "Unsupported board: led1 devicetree alias is not defined"
+#endif
 
-void blue_thread(void *a, void *b, void *c)
+struct printk_data_t
 {
-    ARG_UNUSED(a); ARG_UNUSED(b); ARG_UNUSED(c);
+    void *fifo_reserved; /* 1st word reserved for use by fifo */
+    uint32_t led;
+    uint32_t cnt;
+};
 
-    printk("Blue: started\n");
+K_FIFO_DEFINE(printk_fifo);
 
-    while (1)
-    {
-        gpio_pin_toggle_dt(&blue_led);
-        k_msleep(BLUE_PERIOD_MS); // now truly 500 ms visible
-        printk("Blue: yielding\n");
-        k_thread_resume(&green_thread_data);
-        k_thread_suspend(&blue_thread_data); // clean handoff
-    }
-}
-
-void green_thread(void *dummy1, void *dummy2, void *dummy3)
+struct led
 {
-    ARG_UNUSED(dummy1); ARG_UNUSED(dummy2); ARG_UNUSED(dummy3);
+    struct gpio_dt_spec spec;
+    const char *gpio_pin_name;
+};
 
-    printk("Green: started\n");
+static const struct led led0 = {
+    .spec = GPIO_DT_SPEC_GET_OR(LED0_NODE, gpios, {0}),
+    .gpio_pin_name = DT_PROP_OR(LED0_NODE, label, ""),
+};
 
-    while (1)
-    {
-        if(gpio_pin_get_dt(&green_led))
-        {
-            gpio_pin_set_dt(&green_led, 0); // Turn off LED
-        }
-        else
-        {
-            gpio_pin_set_dt(&green_led, 1); // Turn on LED
-        }
-        k_msleep(GREEN_PERIOD_MS);
-        printk("Green: Resuming blue thread\n");    
-        k_thread_resume(&blue_thread_data);
-        k_thread_suspend(&green_thread_data);
-    }
-}
+static const struct led led1 = {
+    .spec = GPIO_DT_SPEC_GET_OR(LED1_NODE, gpios, {0}),
+    .gpio_pin_name = DT_PROP_OR(LED1_NODE, label, ""),
+};
 
-void main(void)
+void blink(const struct led *led, uint32_t sleep_ms, uint32_t id)
 {
+    const struct gpio_dt_spec *spec = &led->spec;
+    int cnt = 0;
     int ret;
 
-    /* ---------- GPIO setup (must be here!!) ---------- */
-    if (!device_is_ready(blue_led.port) || !device_is_ready(green_led.port))
+    if (!device_is_ready(spec->port))
     {
-        printk("Error: one or both LED devices not ready\n");
+        printk("Error: %s device is not ready\n", spec->port->name);
         return;
     }
 
-    ret = gpio_pin_configure_dt(&blue_led, GPIO_OUTPUT_INACTIVE);   // start OFF
-    ret |= gpio_pin_configure_dt(&green_led, GPIO_OUTPUT_INACTIVE); // start OFF
-    if (ret < 0)
+    ret = gpio_pin_configure_dt(spec, GPIO_OUTPUT);
+    if (ret != 0)
     {
-        printk("Error configuring GPIO pins\n");
+        printk("Error %d: failed to configure pin %d (LED '%s')\n",
+               ret, spec->pin, led->gpio_pin_name);
         return;
     }
 
-    /* ---------- Thread creation ---------- */
-    k_thread_create(&blue_thread_data, blue_thread_stack_area,
-                    K_THREAD_STACK_SIZEOF(blue_thread_stack_area),
-                    blue_thread, NULL, NULL, NULL,
-                    7, 0, K_NO_WAIT); // suspended
+    while (1)
+    {
+        gpio_pin_set(spec->port, spec->pin, cnt % 2);
 
-    k_thread_create(&green_thread_data, green_thread_stack_area,
-                    K_THREAD_STACK_SIZEOF(green_thread_stack_area),
-                    green_thread, NULL, NULL, NULL,
-                    5, 0, K_NO_WAIT); // starts immediately
+        struct printk_data_t tx_data = {.led = id, .cnt = cnt};
 
-    k_thread_name_set(&blue_thread_data, "blue_thread");
-    k_thread_name_set(&green_thread_data, "green_thread");
+        size_t size = sizeof(struct printk_data_t);
+        char *mem_ptr = k_malloc(size);
+        __ASSERT_NO_MSG(mem_ptr != 0);
 
-    printk("main: Green thread started — ping-pong begins!\n");
-        k_thread_start(&green_thread_data);
-        k_thread_start(&blue_thread_data);
+        memcpy(mem_ptr, &tx_data, size);
 
-    while (1) k_msleep(1000);
+        k_fifo_put(&printk_fifo, mem_ptr);
+
+        k_msleep(sleep_ms);
+        cnt++;
+    }
 }
+
+void blink0(void)
+{
+    blink(&led0, 100, 0);
+}
+
+void blink1(void)
+{
+    blink(&led1, 1000, 1);
+}
+
+void uart_out(void)
+{
+    while (1)
+    {
+        struct printk_data_t *rx_data = k_fifo_get(&printk_fifo,
+                                                   K_FOREVER);
+        printk("Toggled led%d; counter=%d\n",
+               rx_data->led, rx_data->cnt);
+        k_free(rx_data);
+    }
+}
+
+K_THREAD_DEFINE(blink0_id, STACKSIZE, blink0, NULL, NULL, NULL,
+                PRIORITY, 0, 0);
+K_THREAD_DEFINE(blink1_id, STACKSIZE, blink1, NULL, NULL, NULL,
+                PRIORITY, 0, 0);
+K_THREAD_DEFINE(uart_out_id, STACKSIZE, uart_out, NULL, NULL, NULL,
+                PRIORITY, 0, 0);
