3d63f085be02e9561573e8c5b025c56a5fd30cee
Mark de Villiers <mfrdevilliers@gmail.com>
2025-11-12 11:26:59 +0800

    EXERCISE: Changed main.c to include thread start in main loop and thread starts and aborts in both threads, the code builds flashes and works as expected. Thread A completes all 10 iterations before Thread B is allowed to work as it should be because Thread A has priority

--- scheduling/coop_time_slice/src/main.c
+++ scheduling/coop_time_slice/src/main.c
@@ -13,21 +13,25 @@
 
 /* Define the stack area for thread A */
 K_THREAD_STACK_DEFINE(stack_A, STACKSIZE);
-// static struct k_thread threadA_data;
+static struct k_thread threadA_data;
 
 /* Define the stack area for thread B*/
 K_THREAD_STACK_DEFINE(stack_B, STACKSIZE);
-// static struct k_thread threadB_data;
+static struct k_thread threadB_data;
 
 void threadA(void *arg1, void *arg2, void *arg3)
 {
     ARG_UNUSED(arg1); ARG_UNUSED(arg2); ARG_UNUSED(arg3);
+    printk("Thread A: Thread A started.\n");
+    k_thread_start(&threadB_data);
+    printk("Thread A: Thread B started by Thread A.\n");
 
     for (int i = 0; i < A_LOOPS; i++) {
-        printk("Hello from Thread A (iteration %d)\n", i + 1);
+        printk("Thread A: Thread A (iteration %d)\n", i + 1);
     
     }
-    printk("Thread A completed all iterations.\n");
+    printk("Thread A: completed all iterations, aborted.\n");
+    k_thread_abort(&threadA_data);
 
 }
 
@@ -35,28 +39,33 @@ void threadB(void *arg1, void *arg2, void *arg3)
 {
     ARG_UNUSED(arg1); ARG_UNUSED(arg2); ARG_UNUSED(arg3);
 
+    printk("Thread B: Thread B started.\n");
+
     for (int i = 0; i < B_LOOPS; i++) {
-        printk("Hello from Thread B (iteration %d)\n", i + 1);
+        printk("Thread B: Thread B (iteration %d)\n", i + 1);
         k_yield();  
     }
-    printk("Thread B completed all iterations.\n");
+    printk("Thread B: completed all iterations, aborted.\n");
+    k_thread_abort(&threadB_data);
 }
 
 void main(void)
 {
     printk("Starting cooperative time slicing demo...\n");
 
-    k_thread_create(NULL, stack_A, STACKSIZE,
+    k_thread_create(&threadA_data, stack_A , STACKSIZE,
                      threadA, NULL, NULL, NULL,
                      A_PRIORITY, 0, K_FOREVER);
-    k_thread_name_set(NULL, "thread_a");
+    k_thread_name_set(&threadA_data, "thread_a");
     
-    k_thread_create(NULL, stack_B , STACKSIZE,
+    k_thread_create(&threadB_data, stack_B , STACKSIZE,
                      threadB, NULL, NULL, NULL,
                      B_PRIORITY, 0, K_FOREVER); 
+    k_thread_name_set(&threadB_data, "thread_b");
 
-    k_thread_start(NULL);
-
+    k_thread_start(&threadA_data);
+    
+    
     // // Optional: keep main thread alive
     // while (1)
     // {
