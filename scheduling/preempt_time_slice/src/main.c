c7594358252b513b0a33974da748ef00ac2d4139
Mark de Villiers <mfrdevilliers@gmail.com>
2025-11-12 15:36:43 +0800

    EXERCISE: Added third thread, compiled successfully, it runs through all iterations, although not quite in the expected order. Still has minor issues

--- scheduling/preempt_time_slice/src/main.c
+++ scheduling/preempt_time_slice/src/main.c
@@ -1,3 +1,7 @@
+/* zephyr-rtos-tutorial - SCHEDULING 
+    EXERCISE - Pre-emptive time slicing*/
+
+
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
@@ -5,11 +9,11 @@
 #define STACKSIZE 1024
 
 /* scheduling priority used by each thread */
-#define A_PRIORITY -2
+#define A_PRIORITY 0
 #define B_PRIORITY -1
+#define C_PRIORITY -2
 
-#define A_LOOPS 10
-#define B_LOOPS 3
+#define LOOPS 3
 
 /* Define the stack area for thread A */
 K_THREAD_STACK_DEFINE(stack_A, STACKSIZE);
@@ -19,6 +23,9 @@ static struct k_thread threadA_data;
 K_THREAD_STACK_DEFINE(stack_B, STACKSIZE);
 static struct k_thread threadB_data;
 
+K_THREAD_STACK_DEFINE(stack_C, STACKSIZE);
+static struct k_thread threadC_data;
+
 void threadA(void *arg1, void *arg2, void *arg3)
 {
     ARG_UNUSED(arg1); ARG_UNUSED(arg2); ARG_UNUSED(arg3);
@@ -26,7 +33,7 @@ void threadA(void *arg1, void *arg2, void *arg3)
     k_thread_start(&threadB_data);
     printk("Thread A: Thread B started by Thread A.\n");
 
-    for (int i = 0; i < A_LOOPS; i++) {
+    for (int i = 0; i < LOOPS; i++) {
         printk("Thread A: Thread A (iteration %d)\n", i + 1);
     
     }
@@ -40,8 +47,10 @@ void threadB(void *arg1, void *arg2, void *arg3)
     ARG_UNUSED(arg1); ARG_UNUSED(arg2); ARG_UNUSED(arg3);
 
     printk("Thread B: Thread B started.\n");
+    k_thread_start(&threadC_data);
+    printk("Thread B: Thread C started by Thread B.\n");
 
-    for (int i = 0; i < B_LOOPS; i++) {
+    for (int i = 0; i < LOOPS; i++) {
         printk("Thread B: Thread B (iteration %d)\n", i + 1);
         k_yield();  
     }
@@ -49,9 +58,23 @@ void threadB(void *arg1, void *arg2, void *arg3)
     k_thread_abort(&threadB_data);
 }
 
+void threadC(void *arg1, void *arg2, void *arg3)
+{
+    ARG_UNUSED(arg1); ARG_UNUSED(arg2); ARG_UNUSED(arg3);
+
+    printk("Thread C: Thread C started.\n");
+
+    for (int i = 0; i < LOOPS; i++) {
+        printk("Thread C: Thread C (iteration %d)\n", i + 1);
+        k_yield();  
+    }
+    printk("Thread C: completed all iterations, aborted.\n");
+    k_thread_abort(&threadC_data);
+}
+
 void main(void)
 {
-    printk("Starting cooperative time slicing demo...\n");
+    printk("Starting preemptive time slicing demo...\n");
 
     k_thread_create(&threadA_data, stack_A , STACKSIZE,
                      threadA, NULL, NULL, NULL,
@@ -63,6 +86,11 @@ void main(void)
                      B_PRIORITY, 0, K_FOREVER); 
     k_thread_name_set(&threadB_data, "thread_b");
 
+    k_thread_create(&threadC_data, stack_C , STACKSIZE,
+                     threadC, NULL, NULL, NULL,
+                     C_PRIORITY, 0, K_FOREVER);
+    k_thread_name_set(&threadC_data, "thread_c");
+    
     k_thread_start(&threadA_data);
     
     
